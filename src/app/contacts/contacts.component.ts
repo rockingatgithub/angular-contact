f7160db3f7f41848198f80980d78ea365469bcf1
rockingatgithub <contactwithsid@gmail.com>
2020-10-25 11:09:22 +0530

    list component added

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+import { CONTACTS } from '../contact-list';
+
+@Component({
+  selector: 'app-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css'],
+})
+export class ContactsComponent implements OnInit {
+  contacts = CONTACTS;
+  selectedContact;
+  onSelect(contact: Contact): void {
+    this.selectedContact = contact;
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
