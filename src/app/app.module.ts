8aab991d6e6af2e2ac26d7d49dce74e82da5636e
rockingatgithub <contactwithsid@gmail.com>
2020-10-25 13:46:15 +0530

    styles added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contact-detail/contact-detail.component';
-import { FilterPipe } from './pipes/filter.pipe'; // -> imported filter pipe
+import { FilterPipe } from './pipes/filter.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; // -> imported filter pipe
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FilterPipe } from './pipes/filter.pipe'; // -> imported filter pipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
